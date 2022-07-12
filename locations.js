c50f362e8e88f71d133059cd8f3f473d21c2d3f0
Joel Alexandre <alexkhang25@yahoo.fr>
2022-07-11 23:47:08 -0500

    update runAuditLocations function

--- locations.js
+++ locations.js
@@ -1,7 +1,7 @@
 const XLSX              =   require("xlsx");
 const { curly }         =   require('node-libcurl');
 
-const COOKIE    = 'cq-authoring-mode=TOUCH; __loc=TX|US; fabrickId=E1:Oh-QFJUIqpR_ZphvJz7PCmtpJ84WlV1zHq57dqvUBRCkkEwZjEag_NtuCGpH93jI04yw5YQQzktA7Vg8tO1hnHrS2oQTE4pvtfiFA5JNK0KS5tKLoV7wZ1SXte6Mt6ictxGaC8sA2Lx3vPoxLag6jw; cq-editor-layer.page=Edit; oauth-configid=ims; login-token=f7a66dfd-d153-45cc-9a82-52a42baa6e2d:5f34583a-bcc2-435e-8ff0-c18442e3b297_17ecc320d2a0d8eb93f845d0ff5d4fb8:crx.default; aem-commonspirit="{2266af00c30339275a5d57c747a92e8569a54f7ec96cb25a6a404c5af1feb85ebc78684019a73becca5e158dcbe13d2bcfc3097ea88c767c6a623695bb8d0770f09ec7a5ea997ec4b4da229cebfeb5aa54dedca64839b09acf2fbadabbad3e919a89bfab7566da3d0d2a69768a101488}"; oauth-authid=ims; AMCVS_8F99160E571FC0427F000101@AdobeOrg=1; lang=en:us; s_cc=true; wcmmode=edit; AMCV_8F99160E571FC0427F000101@AdobeOrg=1687686476|MCIDTS|19185|MCMID|89156023496090199653760977611407146345|MCAAMLH-1658177397|9|MCAAMB-1658177397|RKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y|MCCIDH|-613026243|MCOPTOUT-1657579734s|NONE|MCAID|NONE|vVersion|3.0.0; adcloud={"_les_v":"y,adobecqms.net,1657574399"}; AMCVS_E13D51085E59F02C0A495CDC@AdobeOrg=1; AMCV_E13D51085E59F02C0A495CDC@AdobeOrg=-2121179033|MCIDTS|19185|MCMID|82662739415969301494412556172947609393|MCAAMLH-1658177399|9|MCAAMB-1658177399|RKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y|MCOPTOUT-1657579799s|NONE|MCAID|NONE|vVersion|5.3.0; utag_main=v_id:0181d4682ee0001ff0f496ad57060506f00a306700bd0$_sn:3$_ss:1$_st:1657574400540$vapi_domain:adobecqms.net$dc_visit:3$ses_id:1657572600540;exp-session$_pn:1;exp-session$dc_event:1;exp-session$dc_region:us-east-1;exp-session'
+const COOKIE    = 'cq-authoring-mode=TOUCH; __loc=TX|US; fabrickId=E1:Oh-QFJUIqpR_ZphvJz7PCmtpJ84WlV1zHq57dqvUBRCkkEwZjEag_NtuCGpH93jI04yw5YQQzktA7Vg8tO1hnHrS2oQTE4pvtfiFA5JNK0KS5tKLoV7wZ1SXte6Mt6ictxGaC8sA2Lx3vPoxLag6jw; cq-editor-layer.page=Edit; oauth-configid=ims; oauth-authid=ims; AMCVS_8F99160E571FC0427F000101@AdobeOrg=1; lang=en:us; s_cc=true; wcmmode=edit; AMCV_8F99160E571FC0427F000101@AdobeOrg=1687686476|MCIDTS|19185|MCMID|89156023496090199653760977611407146345|MCAAMLH-1658177397|9|MCAAMB-1658177397|RKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y|MCCIDH|-613026243|MCOPTOUT-1657579734s|NONE|MCAID|NONE|vVersion|3.0.0; adcloud={"_les_v":"y,adobecqms.net,1657574399"}; AMCVS_E13D51085E59F02C0A495CDC@AdobeOrg=1; AMCV_E13D51085E59F02C0A495CDC@AdobeOrg=-2121179033|MCIDTS|19185|MCMID|82662739415969301494412556172947609393|MCAAMLH-1658177399|9|MCAAMB-1658177399|RKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y|MCOPTOUT-1657579799s|NONE|MCAID|NONE|vVersion|5.3.0; utag_main=v_id:0181d4682ee0001ff0f496ad57060506f00a306700bd0$_sn:3$_ss:1$_st:1657574400540$vapi_domain:adobecqms.net$dc_visit:3$ses_id:1657572600540;exp-session$_pn:1;exp-session$dc_event:1;exp-session$dc_region:us-east-1;exp-session; login-token=f7a66dfd-d153-45cc-9a82-52a42baa6e2d:59bc54e8-901c-4629-a35a-555401d2489a_acf2ac5a0c017eaeaa4f4b65ada4434e:crx.default; aem-commonspirit="{36c941922cfd5c6b4e812f4d53c2653b75ae2ad9f3981f6f83b2de731ce7ed2302f5533a10f9b2f63a850656e598767aff20c8ad32c8f39c4174599f258948061489b09338be98b62afb14f9b623285d7b2e82cb8ab987cd3e9e671b39bd02fe5ced6403a1a13faca1b16aad9bd26c20}"'
 const crxde     = "https://author1.stage.commonspirit.adobecqms.net/crx/server/crx.default/jcr%3aroot/content/";
 
 const options = {
@@ -212,53 +212,121 @@ async function exportLocationsData() {
     }
 }
 
-async function runAuditLocations() {
+async function getAllLocations() {
     try {
-        let numberLocations     = 0; 
-        const drupalLocations   = await getLocationsFromDrupal(); 
-
-        console.log(drupalLocations.length);
-
-        let existInDrupalNotInAEM = []; 
-        let existInAEMNotInDrupal = 0; 
-
-        let aemLocations = 0;
-
+        const locationSheets    = [];
         for (let market of availableMarkets) {
             const locations = await getLocations(market.market); 
+
+            if(!locations) {
+                throw new Error(`Unable to get locations for ${market.market}`); 
+            }
+            
             for (const location in locations) {
+                const locationDetails = {}; 
+                const locationIsPublished = await checkIfLocationIsPublished(market.market, location);
+                if (locations[location]['jcr:content'] && locationIsPublished) {
+                    const item          = await getLocationPropreties(market.market, location); 
+                    let itemAddress     = await getLocationAddressPropreties(market.market, location); 
+                    let path            = '';
 
-                if (locations[location]['jcr:content']) {
-                    const locationIsPublished = await checkIfLocationIsPublished(market.market, location);
-
-                    if (locationIsPublished) {
-                        console.log(`This location: ${location} is published ...`);
-                        aemLocations+= 1; 
-                        // check if exist In Drupal
-                        // const item  = await getLocationPropreties(market.market, location);
-                        // console.log(item); 
-                        if (!drupalLocations.find(item => item.propreties.path === `locations/${location}`)) {
-                            existInAEMNotInDrupal += 1;
-                        }
-                        //drupalLocations.forEach((item) => console.log(item))
-
-
-                        // if (existingLocationInDrupal.length ==  0) { 
-                        //     console.log(`This location ${item['jcr:title']} does not exist in Drupal`);
-                        // } else {
-                        //     console.log(`This location ${item['jcr:title']} exist in Drupal`);
-                        // }
+                    if (itemAddress['locationaddressandho']) {
+                        path = 'locationaddressandho';
+                        itemAddress = itemAddress['locationaddressandho'];
+                    } 
+                    if (itemAddress['locationAddressAndHrs']) {
+                        path = 'locationAddressAndHrs';
+                        itemAddress = itemAddress['locationAddressAndHrs'];
+                    } 
+                    if (itemAddress['locationaddressandhr']) {
+                        path = 'locationaddressandhr';
+                        itemAddress = itemAddress['locationaddressandhr'];
                     }
+
+                    locationDetails['Name']     =   (item['jcr:title']) ? item['jcr:title'] : "";
+                    locationDetails['Street']   =   (itemAddress && itemAddress['street']) ? itemAddress['street'] : "";
+                    locationDetails['City']     =   (itemAddress && itemAddress['city']) ? itemAddress['city'] : "";
+                    locationDetails['State']    =   (itemAddress && itemAddress['state']) ? itemAddress['state'] : "";
+                    locationDetails['Zip']      =   (itemAddress && itemAddress['zip']) ? itemAddress['zip'] : "";
+                    locationDetails['Phone']    =   (itemAddress && itemAddress['phone']) ? itemAddress['phone'] : "";
+                    locationDetails['URL']      =   `/locations/${location}`;
+                    locationDetails['Hours']    =   await getLocationHours(market.market, location, path);
+
+                    locationSheets.push(locationDetails); 
                 }
             }
         }
 
-        console.log(aemLocations);
-
-        console.log(existInAEMNotInDrupal);
+        return locationSheets;
 
     } catch (error) {
+        console.log(error.message)
+    }
+}
+
+function getLocationsInAEMNotInDrupal(drupalLocations, aemLocations) {
+    try {
+        const locations = [];
+        for(const aemLocation of aemLocations) {
+            const locationInDrupal = drupalLocations.filter(item => aemLocation['URL'] === item['properties']['path']);
+            if (locationInDrupal.length == 0) {
+                locations.push(aemLocation);
+            }
+        }
+        console.log("Location in AEM not in Drupal :",locations.length);
+        return locations; 
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+function getLocationsInDrupalNotAEM(drupalLocations, aemLocations) {
+    try {
+        const locations = [];
+        for(const drupalLocation of drupalLocations) {
+            const locationInAEM = aemLocations.filter(item => item['URL'] === drupalLocation['properties']['path']);
+            if (locationInAEM.length == 0) {
+                locations.push(drupalLocation);
+            }
+        }
+        console.log("Location In Drupal Not AEM :",locations.length);
+        return locations; 
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+function getLocationsInCommon(drupalLocations, aemLocations) {
+    try {
+        const locations = [];
+        for(const drupalLocation of drupalLocations) {
+            const locationInAEM = aemLocations.filter(item => item['URL'] === drupalLocation['properties']['path']);
+            if (locationInAEM.length > 0) {
+                locations.push(drupalLocation);
+            }
+        }
+        console.log("Location In Common :",locations.length);
+        return locations; 
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+async function runAuditLocations() {
+    try {
+        const drupalLocations           = await getLocationsFromDrupal(); 
+        const aemLocations              = await getAllLocations(); 
+        const locationsInAEMNotDrupal   = getLocationsInAEMNotInDrupal(drupalLocations, aemLocations); 
+        const locationsInDrupalNotAEM   = getLocationsInDrupalNotAEM(drupalLocations, aemLocations);
+        const locationsInCommon         = getLocationsInCommon(drupalLocations, aemLocations);
         
+        const workbook = XLSX.utils.book_new();
+        XLSX.writeFile(workbook, "aem-locations.xlsx");
+    } catch (error) {
+        console.log(error.message)
     }
 }
 
@@ -266,3 +334,7 @@ async function runAuditLocations() {
 //exportLocationsData(); 
 //getLocationsFromDrupal();
 runAuditLocations(); 
+
+// Location in AEM not in Drupal : 105
+// Location in Drupal not aem : 11 
+// Location in Common : 164
